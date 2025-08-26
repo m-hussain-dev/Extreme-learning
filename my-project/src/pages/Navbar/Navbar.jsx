46e359ea5eb1e44c859511180f42c7a620e08fcb
mehdi <mehdihassan78900@gmail.com>
2025-08-26 23:10:17 +0500

    updated

--- my-project/src/pages/Navbar/Navbar.jsx
+++ my-project/src/pages/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="flex flex-wrap justify-between bg-slate-700/55 items-center fixed top-0 left-0 w-full  font-semibold z-50 px-4 py-2">
+    <nav className="flex justify-center rounded-full bg-slate-700/55 items-center fixed top-2 left-1/3 w-auto  font-semibold z-50 px-4 py-2">
       {/* Logo */}
-      <div className="flex items-center">
+      {/* <div className="flex items-center">
         <img src="/doxalogo1.png" alt="logo" className="w-16 md:w-20 lg:w-24" />
-      </div>
+      </div> */}
 
       {/* Mobile Menu Button */}
       <button
@@ -26,10 +26,10 @@ export const Navbar = () => {
         }  md:flex flex-col transition-all h-[100vh] md:h-auto duration-1000 overflow-x-hidden ease-in-out md:flex-row  md:w-auto absolute md:relative top-full left-0 md:top-auto  bg-gray-900/90 md:bg-transparent`}
       >
         <ul className="flex flex-col  md:flex-row items-center gap-4 md:gap-14 w-full md:w-auto py-4 md:py-0">
-          <li className="w-full md:w-auto">
+          <li className=" md:w-auto">
             <Link
               to="/"
-              className="block text-white hover:bg-indigo-600 px-4 py-3 rounded-md text-lg hover:text-slate-300 cursor-pointer text-center"
+              className="block text-white hover:bg-zinc-800 px-3.5 py-1.5 rounded-full text-lg hover:text-slate-300 cursor-pointer text-center"
               onClick={() => setIsOpen(false)}
             >
               Home
@@ -38,7 +38,7 @@ export const Navbar = () => {
           <li className="w-full md:w-auto">
             <Link
               to="/service"
-              className="block text-white hover:bg-indigo-600 px-4 py-3 rounded-md text-lg hover:text-slate-300 cursor-pointer text-center"
+              className="block text-white hover:bg-black px-4 py-3 rounded-full text-lg hover:text-slate-300 cursor-pointer text-center"
               onClick={() => setIsOpen(false)}
             >
               Service
@@ -47,7 +47,7 @@ export const Navbar = () => {
           <li className="w-full md:w-auto">
             <Link
               to="/about"
-              className="block text-white hover:bg-indigo-600 px-4 py-3 rounded-md text-lg hover:text-slate-300 cursor-pointer text-center"
+              className="block text-white hover:bg-black px-4 py-3 rounded-full text-lg hover:text-slate-300 cursor-pointer text-center"
               // onClick={() => setIsOpen(false)}
             >
               About
@@ -56,7 +56,7 @@ export const Navbar = () => {
           <li className="w-full md:w-auto">
             <Link
               to="/contact"
-              className="block text-white hover:bg-indigo-600 px-4 py-3 rounded-md text-lg hover:text-slate-300 cursor-pointer text-center"
+              className="block text-white hover:bg-black px-4 py-3 rounded-full text-lg hover:text-slate-300 cursor-pointer text-center"
               onClick={() => setIsOpen(false)}
             >
               Contact
@@ -66,11 +66,11 @@ export const Navbar = () => {
       </div>
 
       {/* Get Started Button */}
-      <div className="hidden md:inline-block  p-[2px] bg-gradient-to-r from-purple-500 to-pink-500 rounded-md">
+      {/* <div className="hidden md:inline-block  p-[2px] bg-gradient-to-r from-purple-500 to-pink-500 rounded-md">
         <button className=" text-slate-200 px-4 py-2 rounded-md shadow-md w-full hover:bg-indigo-800 transition-colors">
           Get Started
         </button>
-      </div>
+      </div> */}
 
       {/* Mobile Get Started Button */}
       {/* {isOpen && (
